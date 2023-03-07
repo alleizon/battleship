1f78a0edf09ec7a62626c1be043f3fc0e98fa2b7
alleizon <112495304+alleizon@users.noreply.github.com>
2023-03-07 16:42:36 +0200

    added board building page init js & css

---
+++ src/DOM_Helpers/buildPlayerBoard.js
@@ -0,0 +1,78 @@
+const Utils = require("../utils.js");
+
+const BuildBoard = (() => {
+  const renderInfo = () => {
+    const info = document.createElement("div");
+    info.classList.add("info");
+    info.innerHTML = Utils.infoHTML();
+
+    return info;
+  };
+
+  const renderGrid = () => {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+
+    for (let row = 0; row < 10; row += 1) {
+      for (let col = 0; col < 10; col += 1) {
+        const cell = document.createElement("div");
+        cell.dataset.row = row;
+        cell.dataset.col = col;
+        cell.classList.add("grid-cell");
+        grid.appendChild(cell);
+      }
+    }
+
+    return grid;
+  };
+
+  const renderShips = () => {
+    const ships = document.createElement("div");
+    ships.classList.add("ships");
+    ships.innerHTML = Utils.shipsHTML();
+
+    const shipsChildren = Array.from(ships.children).slice(1);
+    shipsChildren.forEach((shipD) => {
+      const target = shipD.children[1];
+      const length = Number(target.dataset.shipLength);
+
+      for (let i = 0; i < length; i += 1) {
+        const shipCell = document.createElement("div");
+        shipCell.classList.add("ship-cell");
+        target.appendChild(shipCell);
+        console.dir(target);
+      }
+    });
+
+    return ships;
+  };
+
+  const renderButtons = () => {
+    const buttons = document.createElement("div");
+    buttons.classList.add("buttons");
+    buttons.innerHTML = Utils.buttonsHTML();
+
+    return buttons;
+  };
+
+  const init = () => {
+    const body = document.querySelector("body");
+    body.id = "build-player-board";
+
+    const title = document.createElement("p");
+    title.textContent = "Build your board";
+
+    // refactor this later
+    const grid = renderGrid();
+    const info = renderInfo();
+    const ships = renderShips();
+    const buttons = renderButtons();
+    body.append(title, ships, grid, info, buttons);
+  };
+
+  return { init };
+})();
+
+BuildBoard.init();
+
+module.exports = BuildBoard;
--- src/utils.js
+++ src/utils.js
@@ -32,7 +32,48 @@ const Utils = (() => {
     </div>
   </div>`;
 
-  return { populateBoards, legendHTML };
+  const infoHTML = () => `<h1>Info</h1>
+  <p>Drag and drop the ships from the right of the grid to build your board.
+    <br>
+    By default, ships are placed horizontaly. Once placed on the board,
+    click on the ship the flip it vertically.
+    <br>
+    Options:
+  </p>
+  <ul>
+    <li>Start (once all ships are placed)</li>
+    <li>Reset</li>
+    <li>Randomise the board</li>
+  </ul>
+</div>`;
+
+  const shipsHTML = () => `<h1>Ships</h1>
+  <div id="carrier">
+    <span data-ship="name">Carrier <span data-ship="amount">1x</span></span>
+    <div class="ship" data-ship-length="5"></div>
+  </div>
+  <div id="battleship">
+    <span data-ship="name">Battleship <span data-ship="amount">1x</span></span>
+    <div class="ship" data-ship-length="4"></div>
+  </div>
+  <div id="cruiser">
+    <span data-ship="name">Cruiser <span data-ship="amount">1x</span></span>
+    <div class="ship" data-ship-length="3"></div>
+  </div>
+  <div id="submarine">
+    <span data-ship="name">Submarine <span data-ship="amount">1x</span></span>
+    <div class="ship" data-ship-length="3"></div>
+  </div>
+  <div id="destroyer">
+    <span data-ship="name">Destroyer <span data-ship="amount">1x</span></span>
+    <div class="ship" data-ship-length="2"></div>
+  </div>`;
+
+  const buttonsHTML = () => `<button class="start" type="button">Start</button>
+  <button class="randomise" type="button">Randomise</button>
+  <button class="reset" type="button">Reset</button>`;
+
+  return { populateBoards, legendHTML, infoHTML, shipsHTML, buttonsHTML };
 })();
 
 module.exports = Utils;
