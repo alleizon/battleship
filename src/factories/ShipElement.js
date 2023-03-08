af060150146a89e2f46fd9f0fc7cd0585b7a0bbb
alleizon <112495304+alleizon@users.noreply.github.com>
2023-03-08 14:38:04 +0200

    added ShipElement class

--- src/factories/ShipElement.js
+++ src/factories/ShipElement.js
@@ -1,5 +1,30 @@
 class ShipElement {
-  constructor() {}
+  constructor(name, length, xOffset, yOffset) {
+    this.name = name;
+    this.length = length;
+    this.left = xOffset * 40 + xOffset;
+    this.top = yOffset * 40 + yOffset;
+    this.element = this.createElement();
+  }
+
+  createElement() {
+    const element = document.createElement("div");
+    element.id = this.name;
+    element.classList.add("placed-ship");
+    element.style.left = `${this.left}px`;
+    element.style.top = `${this.top}px`;
+    this.createChildren(element);
+
+    return element;
+  }
+
+  createChildren(parent) {
+    for (let i = 0; i < this.length; i += 1) {
+      const child = document.createElement("div");
+      child.classList.add("placed-cell");
+      parent.appendChild(child);
+    }
+  }
 }
 
 module.exports = ShipElement;
